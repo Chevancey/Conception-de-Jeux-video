a737443aa8b8cf2120fe990bbfc72111a8965610
Alexandre Viala <57919367+lsacienne@users.noreply.github.com>
2023-09-08 01:16:46 -0400

    :sparkles: Add high score frontend functions

--- Assets/Scripts/HighScores/HighScoreBehaviour.cs
+++ Assets/Scripts/HighScores/HighScoreBehaviour.cs
@@ -11,6 +11,8 @@ public class HighScoreBehaviour : MonoBehaviour
     private TextMeshProUGUI playerText;
     private TextMeshProUGUI scoreText;
     private TextMeshProUGUI dateText;
+
+    private List<GameObject> highscoreObjects = new();
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +30,17 @@ public class HighScoreBehaviour : MonoBehaviour
                         break;
                 }
             }
-        }        
+        }
+        UpdateHighScores();
+    }
+
+    public void ShowHighScore() {
+        gameObject.SetActive(true);
+        UpdateHighScores();
+    }
+
+    public void HideHighScores() {
+        gameObject.SetActive(false);
     }
 
     private void AddLine(string playerName, int score, DateTime date) {
@@ -41,14 +53,50 @@ public class HighScoreBehaviour : MonoBehaviour
         if (dateText != null) {
             dateText.text = date.ToString("dd/MM");
         }
-        Instantiate(lineInstance, gameObject.transform);
+        highscoreObjects.Append(Instantiate(lineInstance, gameObject.transform));
+    }
+
+    private void AddLine(string playerName, int score, string date) {
+        if (playerText != null) {
+            playerText.text = playerName;
+        }
+        if (scoreText != null) {
+            scoreText.text = score.ToString("000000000");
+        }
+        if (dateText != null) {
+            dateText.text = date;
+        }
+        highscoreObjects.Add(Instantiate(lineInstance, gameObject.transform));
+    }
+
+    private void CleanLines() {
+        if (highscoreObjects.Count > 0) {
+            foreach(GameObject highscoreObject in highscoreObjects) {
+                Destroy(highscoreObject);
+            }
+            highscoreObjects.Clear();
+        }
     }
 
     private void BlinkScore(int scoreId) {
 
     }
 
-    private void DisplayHighScores() {
+    private void UpdateHighScores() {
+        HighscoreManager.Instance.UpdateHighScores();
+        HighscoreManager.HighScoreItem[] highscoresItems = HighscoreManager.Instance.ReadHighScores();
+        List<HighscoreManager.HighScoreItem> highscoreList = new(highscoresItems);
+        List<HighscoreManager.HighScoreItem> top8Scores;
+        if (highscoreList.Count > 8) {
+            top8Scores = highscoreList.GetRange(0,8);
+        } else {
+            top8Scores = highscoreList;
+        }
 
+        CleanLines();
+        foreach (HighscoreManager.HighScoreItem scoreItem in top8Scores) {
+            AddLine(scoreItem.name, scoreItem.score, scoreItem.date);
+        }
     }
+
 }
