c2570ab3c68472462a2deee8f5f79dde996fb071
Alexandre Viala <57919367+lsacienne@users.noreply.github.com>
2023-09-08 01:16:17 -0400

    :heavy_minus_sign: Delete unused dependency

--- Assets/Scripts/HighScores/HighScoreBehaviour.cs
+++ Assets/Scripts/HighScores/HighScoreBehaviour.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
-using UnityEngine.TextCore.Text;
 
 public class HighScoreBehaviour : MonoBehaviour
 {
